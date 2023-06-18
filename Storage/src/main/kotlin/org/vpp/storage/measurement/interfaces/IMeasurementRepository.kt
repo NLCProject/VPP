cc764a1703fc0ed262acc3049f8775a588e04deb
Markus Graf <graf.markus10@gmx.at>
2023-06-18 20:42:05 +0200

    Cleanup

--- Storage/src/main/kotlin/org/vpp/storage/measurement/interfaces/IMeasurementRepository.kt
+++ Storage/src/main/kotlin/org/vpp/storage/measurement/interfaces/IMeasurementRepository.kt
@@ -3,7 +3,6 @@ package org.vpp.storage.measurement.interfaces
 import org.springframework.stereotype.Service
 import org.vpp.storage.framework.ICrudlRepository
 import org.vpp.storage.measurement.MeasurementEntity
-import java.util.*
 
 @Service
 interface IMeasurementRepository : ICrudlRepository<MeasurementEntity>
